d5bbda79b7598613850ff475cde1196aef17062c
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-12-06 00:27:33 +0800

    feature
    Webpack

---
+++ src/vueaspnetcore/npmtoturial/src/js/about.js
@@ -0,0 +1,8 @@
+import Vue from 'vue/dist/vue'
+
+var vm = new Vue({
+    el: '#app',
+    data:{
+        message: '哈啰！',
+    },
+});
---
+++ src/vueaspnetcore/npmtoturial/webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry:{
+        index: './src/js/index.js',
+        about: './src/js/about.js'
+    },
+    output:{
+        filename: 'js/[name]/[name].js',
+        chunkFilename: 'js/[name].js'
+    },
+    optimization:{
+        splitChunks:{
+            chunks: 'all',
+            name: 'vendor'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'pages/index.html',
+            chunks: ['index', 'vendor'],
+            inject: true
+        })
+    ]
+}
