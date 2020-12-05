ec050fc1dfce4caab3addad4ec39aa0d6d289d86
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-12-05 16:40:55 +0800

    feature
    Vue概览

---
+++ src/uniapp/test/components/uni-number-box/mycomponent.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<h1>{{title}}</h1>
+		<div @tap="tap1">
+			{{content}}
+		</div>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			title: {
+				type: String,
+				default: ""
+			},
+			content: {
+				type: String,
+				default: ""
+			}
+		},
+		data(){
+			return{
+				
+			};
+		},
+		methods:{
+			tap1: function(){
+				console.log(1);
+				this.$emit('tap1', this.title);
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ src/vueui/test/src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        Hello WOrld!
+    </div>
+</template>
+<script>
+export default {
+    name: 'Test',
+    props:{
+        type: String,
+        default: "Hello WOrld!"
+    },
+    methods: {
+        
+    },
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+    .red{color:#FF0000}
+</style>
