92f7208dffb10b77cef12b83c752f670ec3be9ef
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-12-06 01:01:40 +0800

    feature
    Webpack入门

--- src/vueaspnetcore/npmtoturial/webpack.config.js
+++ src/vueaspnetcore/npmtoturial/webpack.config.js
@@ -1,4 +1,17 @@
+const glob = require('glob')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+var htmlPages = glob.sync('./src/*.html')
+    .map(path => {
+        // path example: ./src/index.html
+        var name = path.replace('./src/', '').replace('.html', '');
+        return new HtmlWebpackPlugin({
+            template: path,
+            filename: `pages/${name}.html`,
+            chunks: [name, 'vendor'],
+            inject: true
+        });
+    })
 
 module.exports = {
     entry:{
@@ -16,11 +29,17 @@ module.exports = {
         }
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'pages/index.html',
-            chunks: ['index', 'vendor'],
-            inject: true
-        })
-    ]
+        // new HtmlWebpackPlugin({
+        //     template: './src/index.html',
+        //     filename: 'pages/index.html',
+        //     chunks: ['index', 'vendor'],
+        //     inject: true
+        // }),
+        // new HtmlWebpackPlugin({
+        //     template: './src/about.html',
+        //     filename: 'pages/about.html',
+        //     chunks: ['about', 'vendor'],
+        //     inject: true
+        // })
+    ].concat(htmlPages)
 }
